b10031cd253ae42fb7928c63864a261d195f6c14
Thoufeek19 <thoufeek12604@gmail.com>
2025-05-31 10:27:21 +0530

    Time: 0 ms (100%), Space: 17.7 MB (78.63%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack=[]
+        for i in s:
+            if i.isdigit():
+                if stack and not stack[-1].isdigit():
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+        return ''.join(stack)
