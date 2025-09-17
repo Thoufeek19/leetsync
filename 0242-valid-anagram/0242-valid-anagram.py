0851d1c8bab88648c7539ef52037b797b4f8888d
Thoufeek19 <thoufeek12604@gmail.com>
2025-09-17 23:25:23 +0530

    Time: 11 ms (72.46%), Space: 18.1 MB (34.07%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d={}
+        d1={}
+        for i in s:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i in t:
+
+            if i in d1:
+                d1[i]+=1
+            else:
+                d1[i]=1
+        return d==d1
+        
