d7495aea7e3b1d42852e0da5b954390dfd64a666
Thoufeek19 <thoufeek12604@gmail.com>
2025-09-28 00:43:35 +0530

    Time: 16 ms (76.85%), Space: 18.1 MB (55.72%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.py
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -1,25 +1,50 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
-        d={}
-        ans = -1
-        max_count = 0
-        for i in nums:
-            # even & new
-            if i % 2 == 0:
-                if i not in d:
-                    d[i] = 1
-                    if d[i] > max_count:
-                        ans = i
-                        max_count = d[i]
-                else:
-                    d[i] += 1
-                    if d[i] > max_count:
-                        ans = i
-                        max_count = d[i]
-                    elif d[i] == max_count:
-                        ans = min(ans, i)
+        # d={}
+        # ans = -1 # elements (key)
+        # max_count = 0 # max count (value)
         
-        return ans
+        # for i in nums:
+        #     # even & new
+        #     if i % 2 == 0:
+        #         if i not in d:
+        #             d[i] = 1
+        #             if d[i] > max_count:
+        #                 ans = i
+        #                 max_count = d[i]
+        #             elif d[i] == max_count:
+        #                 ans = min(ans, i)
+        #         else:
+        #             d[i] += 1
+        #             if d[i] > max_count:
+        #                 ans = i
+        #                 max_count = d[i]
+        #             elif d[i] == max_count:
+        #                 ans = min(ans, i)
+        
+        # return ans
                 
-            
+        # 2nd approach
+        d = {}
+        ans = -1
+        for i in nums:
+            if i % 2 != 0:
+                continue
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] += 1
+        
+        arr = []
+        if len(d) > 0:
+            max_value = max(d.values())
+        else:
+            return ans
+        for i in d:
+            if d[i] == max_value:
+                arr.append(i)
+        
+        if len(arr) > 0:
+            ans = min(arr)
+        return ans
         
